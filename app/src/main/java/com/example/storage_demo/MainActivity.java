292cb5fb71227943fb52c6cfa35b164ac5d9067a
Dream-By <dzmitry.dzianisau@gmail.com>
2020-06-18 16:00:48 +0300

    add exception

--- app/src/main/java/com/example/storage_demo/MainActivity.java
+++ app/src/main/java/com/example/storage_demo/MainActivity.java
@@ -1,7 +1,16 @@
 package com.example.storage_demo;
 
+import android.content.Context;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +19,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+        Button saveFileButton = (Button)findViewById(R.id.button_save);
+        final EditText fileNameEditText = (EditText)findViewById(R.id.edit_filename);
+        final EditText contentEditText = (EditText)findViewById(R.id.edit_content);
+
+        saveFileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String fileName = fileNameEditText.getText().toString();
+                String content = contentEditText.getText().toString();
+
+                FileOutputStream fos;
+                try {
+                    fos = openFileOutput(fileName, Context.MODE_PRIVATE);
+                    fos.write(content.getBytes());
+                    fos.close();
+
+                    Toast.makeText(getApplicationContext(),"Файл " + fileName + " сохранен", Toast.LENGTH_LONG).show();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
